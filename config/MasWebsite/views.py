a5f21325e54ae8201a49e0f7ad70cf1531bf3868
Jeffrey Soto <femtography@me.com>
2021-03-31 15:18:16 -0600

    brought in home about and contact pages

--- config/MasWebsite/views.py
+++ config/MasWebsite/views.py
@@ -2,3 +2,23 @@ from django.shortcuts import render
 
 def index (request):
     return render(request, 'MasWebsite/index.html')
+
+def about(request):
+    return render(request, 'MasWebsite/about.html')
+
+def contact(request):
+    if request.method == 'POST':
+        message = request.POST['message']
+        name = request.POST['user_name']
+        email = request.POST['user_email']
+        category = request.POST['category']
+
+        subject = name.capitalize() + ' is reaching out from ' + email + ' about ' + category
+
+        send_mail(subject,
+        message,
+        settings.EMAIL_HOST_USER,
+        ['gcflores0303@gmail.com'],
+        fail_silently=False)
+
+    return render(request, 'MasWebsite/contact.html')

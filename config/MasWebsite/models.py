7df097ced1b43eca4712c7790b3e48393681364d
Jeffrey Soto <femtography@me.com>
2021-04-04 16:44:44 -0600

    updated product objects display name in admin site

--- config/MasWebsite/models.py
+++ config/MasWebsite/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 class Product(models.Model):
+    def __str__(self):
+         return self.name
 
     name = models.CharField(max_length=30)
     short_d = models.CharField(max_length=120)
     category = models.CharField(max_length=15)
     url = models.CharField(max_length=100)
     is_featured = models.BooleanField(default=False)
- 
